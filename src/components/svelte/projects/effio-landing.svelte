628211aa304928b8b0e79ffa35dc9a730ece2e24
matej-kotrba <matejkotrba.skola@gmail.com>
2024-07-21 13:30:49 +0200

    Update landing page with new background effect animation and lazy loading for images

--- src/components/svelte/projects/effio-landing.svelte
+++ src/components/svelte/projects/effio-landing.svelte
@@ -2,8 +2,15 @@
   import { onMount } from "svelte";
   import gsap from "gsap";
   import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+  import * as THREE from "three";
+  import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
   const backgroundEffectText = "EFFIO";
+
+  // Three animation data
+  // const sizes = [500, 800];
+
+  let mobileCanvas: HTMLCanvasElement;
 
   onMount(() => {
     gsap.registerPlugin(ScrollTrigger);
@@ -33,7 +40,7 @@
         end: "end top",
         scrub: 1,
       },
-      translateX: 0,
+      translateX: 40,
     })
       .to("#effio2", {
         scrollTrigger: {
@@ -60,12 +67,62 @@
           end: "top bottom-=200px",
           scrub: 1,
         },
-        translateX: 0,
+        translateX: -40,
       });
+  });
+
+  onMount(() => {
+    const sizes = [window.innerWidth, window.innerHeight];
+
+    const loader = new GLTFLoader();
+    let mobileModel: THREE.Object3D<THREE.Object3DEventMap>;
+
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xdddddd);
+
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      sizes[0] / sizes[1],
+      0.1,
+      1000
+    );
+
+    let hlight = new THREE.AmbientLight(0x404040, 100);
+    scene.add(hlight);
+
+    const renderer = new THREE.WebGLRenderer({ canvas: mobileCanvas });
+    renderer.setSize(sizes[0], sizes[1]);
+
+    camera.position.z = 10;
+
+    function animate() {
+      if (mobileModel) {
+        mobileModel.rotation.y += 0.005;
+      }
+
+      renderer.render(scene, camera);
+    }
+    renderer.setAnimationLoop(animate);
+
+    loader.load(
+      "/models/mobile.glb",
+      (gtlf) => {
+        scene.add(gtlf.scene);
+        mobileModel = gtlf.scene.children[0];
+        // object.scale.x = 1;
+        // object.scale.y = 1;
+        // object.scale.z = 1;
+        // scene.add(object);
+      },
+      undefined,
+      (err) => {
+        console.error(err);
+      }
+    );
   });
 </script>
 
-<div class="relative py-[200px] grid place-content-center isolate">
+<!-- <div class="relative py-[200px] grid place-content-center isolate">
   <div
     class="absolute text-landing-bg flex flex-col leading-[80%] items-center rotate-[20deg] opacity-5 -translate-x-1/2 left-1/2 select-none z-[-99]"
   >
@@ -78,4 +135,5 @@
   </div>
   <h3 class="text-landing text-center leading-[1]">Effio</h3>
   <p class="text-text-darker text-h3 leading-[1]">Online Test Creation Tool</p>
-</div>
+</div> -->
+<canvas id="mobile-canvas" bind:this={mobileCanvas}></canvas>
